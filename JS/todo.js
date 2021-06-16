744a7f28968d22653a6aad8198155a67021e8a4e
huyntohoon <81351526+huyntohoon@users.noreply.github.com>
2021-06-16 16:46:06 +0900

    work_6/16

--- JS/todo.js
+++ JS/todo.js
@@ -2,11 +2,17 @@ const toDoForm = document.querySelector(".todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".todo-list");
 
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
 }
-
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
@@ -24,7 +30,19 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    console.log(newTodo);
+    toDos.push(newTodo);
+    console.log(toDos);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo); 
+}
